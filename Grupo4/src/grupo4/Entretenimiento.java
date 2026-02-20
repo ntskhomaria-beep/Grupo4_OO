d80266b17b38f941c26e0d362517075ab712d311
n.tskhomaria <n.tskhomaria@PC15-A14.CEMK.LOCAL>
2026-02-20 09:15:09 +0100

    Creacion de la clase padre "Entretenimiento" y asignación de sus atributos

---
+++ Grupo4/src/grupo4/Entretenimiento.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package grupo4;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author n.tskhomaria
+ */
+public class Entretenimiento {
+  private String titulo; 
+  private int audiencia; 
+  private String empresa; 
+  private double critica; 
+  private String plataforma; 
+  private ArrayList<String>patrocinador;
+}
+
+
+
+
