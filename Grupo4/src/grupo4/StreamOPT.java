8570658ed08001f7bce012d85d373672f789b4e2
E.JUEZ <E.JUEZ@PC09-A14.CEMK.LOCAL>
2026-02-27 10:16:32 +0100

    Modificar y crear el esquema del main del proyecto

--- Grupo4/src/grupo4/StreamOPT.java
+++ Grupo4/src/grupo4/StreamOPT.java
@@ -12,32 +12,40 @@ import java.util.Scanner;
  */
 public class StreamOPT {
     public static void main(String[] args) {
-        System.out.println("Bienvenido! Aqui podras ver varias peliculas, series y estreams de tus personas y actores "
-                + "favorito.");
+        System.out.println("Bienvenido! Aqui podras ver varias peliculas, series y estreams de tus personas y "
+                + "actores favorito.");
         System.out.println();
-        System.out.println("1. Peliculas");
-        System.out.println("2. Series");
-        System.out.println("3. Streams");
+        System.out.println("1. Iniciar Sesion");
+        System.out.println("2. Peliculas");
+        System.out.println("3. Series");
+        System.out.println("4. Streams");
         Scanner entrar = new Scanner(System.in);
         String menu = entrar.nextLine();
+        
         switch(menu){
             case "1":
-            case "peliculas":
-            case "Peliculas":{
-                System.out.println();
-            }
+                case "Iniciar sesion":
+                case "IniciarSesion":{
+                    System.out.println();
+                }
             
             case "2":
-            case "Series":
-            case "series":{
-                
-            }
+                case "peliculas":
+                case "Peliculas":{
+                    System.out.println();
+                }
             
             case "3":
-            case "streams":
-            case "Streams":{
-                
-            }
+                case "Series":
+                case "series":{
+                    System.out.println();
+                }
+            
+            case "4":
+                case "streams":
+                case "Streams":{
+                    System.out.println();
+                }
         }
     }
 }
